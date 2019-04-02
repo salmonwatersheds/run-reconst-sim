15d9766bf509df9d0944272b4ce4a54e513ec9bd
Stephanie J Peacock <stephanie.j.peacock@gmail.com>
2019-04-01 19:52:27 -0600

    Added proper documentation to runSensitivity.R functions

--- model/runSensitivity.R
+++ model/runSensitivity.R
@@ -1,3 +1,23 @@
+#______________________________________________________________________________
+#' Make list of parameter sets to be run in a sensitivity analysis 
+#' 
+#' This function takes a numeric vector of values for a given parameter in
+#' `simPar` and returns a list of parameter sets for input into
+#' `runSensitivity()`.
+#' 
+#' @param basePar A named vector of the base parameter values to be applied.
+#' @param sensName The character name of the parameter that is being varied,
+#' matching exactly as the name appears in `basePar`
+#' @param sensValues A vector of values for the parameter named in sensName
+#' over which the sensitivity analysis is to be run.
+#' @return Returns a list, where each element is a unique parameter combination
+#' to be run in a sensitivity analysis. 
+#' 
+#' @details 
+#' 
+#' @examples 
+#' 
+
 makeParList <- function(basePar = simPar, sensName = "obs_bias", sensValues = log(1/seq(1, 5, 0.2))){
 	
 	# Check that parameter to vary is correctly named in basePar
@@ -14,6 +34,36 @@ makeParList <- function(basePar = simPar, sensName = "obs_bias", sensValues = lo
 	
 	return(parList)
 }
+
+#______________________________________________________________________________
+#' Run a sensitivitity analysis over a list of parameter sets 
+#' 
+#' This function takes a list of parameters and runs multiple MC simulations
+#' of `reconstrSim()` for each parameter set (i.e., element) in that list.
+#' 
+#' @param parList A list of parameter sets, where each element is a named
+#' vector of all parameters being fed into `reconstrSim()`. Can be the output
+#' of `makeParList()` above.
+#' @param nSim The number of MC simulations to be run for each parameter set
+#' @param nCores THe number of cores over which to run the MC simulations in
+#' parallel.
+#' @return Returns a list, with the first element equal to the overall output 
+#' of the sensitivity analysis (see Details) and the second element equal to
+#' the total time the simulation took in minutes.
+#' 
+#' @details The overall output is given as a list, where each element is the
+#' performace output for the corresponding parameter set in the given `parList`.
+#' The performace is itself a list, where:
+#' 1. the first element is the relative bias (RB, a.k.a. mean percent error 
+#' MPE) in the current spawner abundance, 
+#' 2. the second element is the RB in the stock-recruit benchmarks and the 
+#' (mis)classification of status (as a numebr from 1-8; see documentation for
+#' `perfStatus()`, each given as columns in a matrix with rows equal to `nSim`,
+#' 3. the third element is the RB and (mis)classification according to the
+#' historical spawners metric.
+#' 
+#' @examples 
+#' 
 
 runSensitivity <- function(parList, nSim, nCores){
 	
