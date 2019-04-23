74b550963780a1811eca15fba1a3a29b1855cae4
Stephanie J Peacock <stephanie.j.peacock@gmail.com>
2019-04-23 16:23:14 -0600

    Added plot looking at temporal trends in HCR residuals.

--- data/harvestRate.R
+++ data/harvestRate.R
@@ -5,7 +5,7 @@ library(RColorBrewer)
 ###############################################################################
 
 CU_names <- read.csv("data/CentralCoastChum_CUs.csv")
-CU_names <- as.character(CU_names[1:8,1]) #Ignore Wannock (no data)
+CU_names <- as.character(CU_names[1:8,'CU_name']) #Ignore Wannock (no data)
 
 datER <- read.csv("data/PSE_chumExploitationRate.csv")
 datER <- subset(datER, is.element(datER$location, CU_names))
@@ -88,10 +88,10 @@ I <- 1
 												 
 for(i in c(1,3:8)){
 	dat.i <- data.frame(
-		y = subset(datER, datER$location == CU_names[i])$datavalue, 
+		y = subset(datER, datER$location == CU_names[i])$datavalue,
 		x = subset(datTotalRun, datTotalRun$location == CU_names[i])$datavalue,
-		esc = subset(datAbundLGL, datAbundLGL$location == CU_names[i])$datavalue, 
-		year = subset(datER, datER$location == CU_names[i])$year) 
+		esc = subset(datAbundLGL, datAbundLGL$location == CU_names[i])$datavalue,
+		year = subset(datER, datER$location == CU_names[i])$year)
 
 	dat.all$location[I:(I + dim(dat.i)[1] - 1)] <- CU_names[i]
 	dat.all$year[I:(I+dim(dat.i)[1]-1)] <- dat.i$year
@@ -100,50 +100,50 @@ for(i in c(1,3:8)){
 	I <- I + dim(dat.i)[1]
 }
 
-	xmax <- ceiling(max(dat.i$x)*10^-5)*10^5
-	
-	# Fit curve
-	fit.i <- nls(
-		y ~ hmax * (1 - exp(d * (m - x))), 
-		data = dat.i, 
-		start = list(hmax = 0.8, d = 1.2*10^-6, m = 10^5), 
-		lower = c(0, 0, 0), 
-		upper = c(1, Inf, 10^6), 
-		algorithm = "port")
-	harvestPar[i,'hmax'] <- coefficients(fit.i)['hmax']
-	harvestPar[i,'d'] <- coefficients(fit.i)['d']
-	harvestPar[i,'m'] <- coefficients(fit.i)['m']
-	
-	R.dummy <- seq(0, max(dat.i$x), length.out=200)
-	h.dummy <- coefficients(fit.i)['hmax'] * (1 - exp(coefficients(fit.i)['d'] * (coefficients(fit.i)['m'] - R.dummy)))
-	
-	
-	par(mfrow=c(2,1), mar=c(3,5,2,5), oma=c(1,0,0,0))
-	bp <- barplot(rbind(dat.i$x - dat.i$esc, dat.i$esc), names.arg = NULL, las=1, col=c("#A8A6FF", "#FF0200"), yaxt="n", ylim=c(0, xmax), main=CU_names[i])
-	axis(side=1, at=bp[match(seq(1960, 2010, 10), dat.i$year)], labels = seq(1960, 2010, 10))
-	
-	xdist <- 100000
-	while(length(seq(0, xmax, xdist)) > 8) xdist <- xdist * 2
-	axis(side=2, at=seq(0, xmax, xdist), las=1, labels = formatC(seq(0, xmax/1000, xdist/1000), 0, format="f"))
-	
-	mtext(side=2, "Escapement + harvest (1000s)", line=3)
-	lines(bp, dat.i$y*xmax, lwd=1.5)
-	axis(side=4, at=seq(0, 1, 0.2)*xmax, labels=seq(0, 1, 0.2), las=1)
-	mtext(side=4, "Exploitation rate", line=3)
-	mtext(side=3, line=1, adj=0, "a)")
-	
-	plot(dat.i$x, dat.i$y, las=1, ylab="Exploitation rate", xlab="Escapement + harvest", bty="l", xlim=c(0, xmax), ylim=c(0,1), xaxs="i", yaxs="i", xaxt="n")
-	axis(side=1, at=seq(0, xmax, xdist), las=1, labels = formatC(seq(0, xmax/1000, xdist/1000), 0, format="f"))
-	mtext(side=1, "Escapement + harvest (1000s)", line=3)
-	
-	mtext(side=3, line=1, adj=0, "b)")
-	
-	lines(R.dummy, h.dummy, lwd=1.5)
-	abline(h = coefficients(fit.i)['hmax'], lty=3)
-	abline(v = coefficients(fit.i)['m'], lty=3)
-}
-
-
+# 	xmax <- ceiling(max(dat.i$x)*10^-5)*10^5
+# 
+# 	# Fit curve
+# 	fit.i <- nls(
+# 		y ~ hmax * (1 - exp(d * (m - x))),
+# 		data = dat.i,
+# 		start = list(hmax = 0.8, d = 1.2*10^-6, m = 10^5),
+# 		lower = c(0, 0, 0),
+# 		upper = c(1, Inf, 10^6),
+# 		algorithm = "port")
+# 	harvestPar[i,'hmax'] <- coefficients(fit.i)['hmax']
+# 	harvestPar[i,'d'] <- coefficients(fit.i)['d']
+# 	harvestPar[i,'m'] <- coefficients(fit.i)['m']
+# 
+# 	R.dummy <- seq(0, max(dat.i$x), length.out=200)
+# 	h.dummy <- coefficients(fit.i)['hmax'] * (1 - exp(coefficients(fit.i)['d'] * (coefficients(fit.i)['m'] - R.dummy)))
+# 
+# 
+# 	par(mfrow=c(2,1), mar=c(3,5,2,5), oma=c(1,0,0,0))
+# 	bp <- barplot(rbind(dat.i$x - dat.i$esc, dat.i$esc), names.arg = NULL, las=1, col=c("#A8A6FF", "#FF0200"), yaxt="n", ylim=c(0, xmax), main=CU_names[i])
+# 	axis(side=1, at=bp[match(seq(1960, 2010, 10), dat.i$year)], labels = seq(1960, 2010, 10))
+# 
+# 	xdist <- 100000
+# 	while(length(seq(0, xmax, xdist)) > 8) xdist <- xdist * 2
+# 	axis(side=2, at=seq(0, xmax, xdist), las=1, labels = formatC(seq(0, xmax/1000, xdist/1000), 0, format="f"))
+# 
+# 	mtext(side=2, "Escapement + harvest (1000s)", line=3)
+# 	lines(bp, dat.i$y*xmax, lwd=1.5)
+# 	axis(side=4, at=seq(0, 1, 0.2)*xmax, labels=seq(0, 1, 0.2), las=1)
+# 	mtext(side=4, "Exploitation rate", line=3)
+# 	mtext(side=3, line=1, adj=0, "a)")
+# 
+# 	plot(dat.i$x, dat.i$y, las=1, ylab="Exploitation rate", xlab="Escapement + harvest", bty="l", xlim=c(0, xmax), ylim=c(0,1), xaxs="i", yaxs="i", xaxt="n")
+# 	axis(side=1, at=seq(0, xmax, xdist), las=1, labels = formatC(seq(0, xmax/1000, xdist/1000), 0, format="f"))
+# 	mtext(side=1, "Escapement + harvest (1000s)", line=3)
+# 
+# 	mtext(side=3, line=1, adj=0, "b)")
+# 
+# 	lines(R.dummy, h.dummy, lwd=1.5)
+# 	abline(h = coefficients(fit.i)['hmax'], lty=3)
+# 	abline(v = coefficients(fit.i)['m'], lty=3)
+# }
+# 
+# 
 
 dat.all <- dat.all[1:(which(is.na(dat.all$location)==TRUE)[1]-1),]
 
@@ -160,23 +160,45 @@ h.dummy <- 0.43 * (1 - exp(1.15*10^-5 * (1967 - R.dummy)))
 h.dummy2 <- 0.4 * (1 - exp(1.81*10^-5 * (1405 - R.dummy)))
 h.dummy3 <- coefficients(fit.all)['hmax'] * (1 - exp(coefficients(fit.all)['d'] * (coefficients(fit.all)['m'] - R.dummy)))
 
-quartz(width = 6.3, height=2.8, pointsize = 9)
-par(mfrow=c(1,1), mar=c(4,4,2,10), oma=c(0,0,0,0))
+quartz(width = 6, height=2.8, pointsize = 9)
+par(mfrow=c(1,1), mar=c(4,4,2,15), oma=c(0,0,0,0))
 plot(
 	dat.all$totalRun[dat.all$location != "Smith Inlet" & dat.all$location != "Rivers Inlet"], 
 	dat.all$harvestRate[dat.all$location != "Smith Inlet" & dat.all$location != "Rivers Inlet"], 
-	col=as.numeric(as.factor(dat.all$location[dat.all$location != "Smith Inlet" & dat.all$location != "Rivers Inlet"])), 
+	col = grey(0.7), #"#00000050",#col=as.numeric(as.factor(dat.all$location[dat.all$location != "Smith Inlet" & dat.all$location != "Rivers Inlet"])), 
 	las=1, 
-	xlab="Total return (escapement + harvest)", 
+	xlab="Total return (millions)", 
 	ylab = "Harvest rate", 
 	bty="l", 
-	pch = as.numeric(as.factor(dat.all$location[dat.all$location != "Smith Inlet" & dat.all$location != "Rivers Inlet"])))
-legend(1.8*10^6, 0.85, col=unique(as.numeric(as.factor(dat.all$location[dat.all$location != "Smith Inlet" & dat.all$location != "Rivers Inlet"]))), pch = unique(as.numeric(as.factor(dat.all$location[dat.all$location != "Smith Inlet" & dat.all$location != "Rivers Inlet"]))), legend= unique(dat.all$location[dat.all$location != "Smith Inlet" & dat.all$location != "Rivers Inlet"]), xpd=NA)
+	pch = as.numeric(as.factor(dat.all$location[dat.all$location != "Smith Inlet" & dat.all$location != "Rivers Inlet"])),
+	xaxt="n", yaxt="n")
+	axis(side=1, at=seq(0, 1.5*10^6, 0.5*10^6), labels = c("0.0", "0.5", "1.0", "1.5"))
+	axis(side=2, at=c(0, 0.3, 0.6), las=1)
+	axis(side=2, at=coefficients(fit.all)['hmax'], labels=expression(paste(italic(h[MAX]))), las=1, cex.axis=0.8, col="dodgerblue", col.axis="dodgerblue")
+legend(2*10^6, 0.85, col=grey(0.7), pch = unique(as.numeric(as.factor(dat.all$location[dat.all$location != "Smith Inlet" & dat.all$location != "Rivers Inlet"]))), legend= unique(dat.all$location[dat.all$location != "Smith Inlet" & dat.all$location != "Rivers Inlet"]), xpd=NA)
 lines(R.dummy, h.dummy3, lwd=1.5)
-abline(h = coefficients(fit.all)['hmax'], lty=2)
+abline(h = coefficients(fit.all)['hmax'], lty=2, col="dodgerblue")
 
-lines(R.dummy, h.dummy)
-lines(R.dummy, h.dummy2, lty=2)
+# lines(R.dummy, h.dummy)
+# lines(R.dummy, h.dummy2, lty=2)
 
 
+#-----------------------------
+# Plot residuals over time
+#-----------------------------
+dat.fit <- dat.all[dat.all$location != "Smith Inlet" & dat.all$location != "Rivers Inlet",]
+res <- resid(fit.all)
+
+par(mfrow=c(1,2), mar=c(4,4,2,1), oma=c(0,0,0,0))
+
+plot(dat.fit$totalRun, dat.fit$harvestRate, col = grey(0.6), las=1, xlab="Total return (millions)", ylab = "Harvest rate", bty="l", pch = as.numeric(as.factor(dat.fit$location)), xaxt="n", yaxt="n")
+axis(side=1, at=seq(0, 1.5*10^6, 0.5*10^6), labels = c("0.0", "0.5", "1.0", "1.5"))
+axis(side=2, at=c(0, 0.3, 0.6), las=1)
+axis(side=2, at=coefficients(fit.all)['hmax'], labels=expression(paste(italic(h[MAX]))), las=1, cex.axis=0.8, col="dodgerblue", col.axis="dodgerblue")
+lines(R.dummy, h.dummy3, lwd=1.5)
+mtext(side = 3, adj=0, line=0.5, "a) HCR fit")
+
+plot(dat.fit$year, res, las=1, ylab="Residuals", xlab = "Return year", bty="l", pch = as.numeric(as.factor(dat.fit$location)), col=grey(0.6))
+abline(h=0, lwd=1.5)
+mtext(side = 3, adj=0, line=0.5, "b) Residuals from HCR fit")
 
